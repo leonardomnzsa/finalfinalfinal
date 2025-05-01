8a95892c17dc9a512f7403481685050d3a0528f0
leonardomnzsa <leonardomnzsa@gmail.com>
2025-05-01 12:16:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -378,7 +378,6 @@ else:
             selected_rg = 'Todos'
             show_unread_only = False
             date_filter_type = "Ano"
-no"
 
 # --- Main Content Area --- 
 if df_informativos_exploded is not None and df_informativos_original is not None:
